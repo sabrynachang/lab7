d6fcfc28755ea6f112e522e1e3ba3b20dd83a744
Sabryna Chang <latte.yay@gmail.com>
2016-02-18 05:35:19 +0000

    remove coin flip

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,18 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
-    var random_num = Math.random();
-    console.log(random_num);
+    // var random_num = Math.random();
+    // console.log(random_num);
 
-    if (random_num > 0.5) {
-      projects["grid"] = false;
-      res.render('index', projects);
-    } else {
-      res.redirect('/grid');
-    }
+    // if (random_num > 0.5) {
+    //   projects["grid"] = false;
+    //   res.render('index', projects);
+    // } else {
+    //   res.redirect('/grid');
+    // }
+
+  projects["grid"] = false;
+  res.render('index', projects);
 
 };
 
